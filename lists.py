92276e9265ef77c06811908540fb64b7e1a50097
mr_sd_jack_003 <barson0habra@gmail.com>
2023-03-15 01:24:28 +0530

    add list file

---
+++ lists.py
@@ -0,0 +1,11 @@
+friends = ["Soham","Subir","Somnath"]
+#             0       1        2
+
+friends[1]="Mike"
+print(friends[1])
+
+print(friends[2])
+print(friends[-2])
+print(friends[0])
+print(friends[-1])
+print(friends[1:])
