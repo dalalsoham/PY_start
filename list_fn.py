a14c3a89840dea9e5f7d543ff326cf34d2a1b780
mr_sd_jack_003 <barson0habra@gmail.com>
2023-03-15 01:36:30 +0530

    add list fn

---
+++ list_fn.py
@@ -0,0 +1,18 @@
+lucky_number = [4,8,15,16,23,42]
+friends=["Soham", "subir","somnath","somnath","somnath","sudip","sayan"]
+
+# friends.extend(lucky_number)
+# friends.append("Creed")
+# friends.insert(1, "Kelly")
+# friends.remove("sayan")
+# friends.clear()
+# friends.pop()
+# print(friends.index("somnath"))
+# print(friends.count("somnath"))
+# friends.sort()
+# lucky_number.sort()
+# lucky_number.reverse()
+# print(lucky_number)
+# friends2=friends.cpoy()
+# print(friends2)
+# print(friends)
