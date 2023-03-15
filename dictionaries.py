3eb02c12091e8f7e85d936280049ef779df83057
mr_sd_jack_003 <barson0habra@gmail.com>
2023-03-15 23:51:40 +0530

    dectionaries

---
+++ dictionaries.py
@@ -0,0 +1,21 @@
+# jan -> january
+# mar -> march
+
+monthconversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+
+# print(monthconversions["Mar"])   ->>> 1st way to do that
+# print(monthconversions.get("Dec"));  ->>> 2nd way to do that 
