d1e743859cde3bfd47b01e9705012504b16e9f71
mr_sd_jack_003 <barson0habra@gmail.com>
2023-03-15 23:20:33 +0530

    if statements and comparisons

---
+++ if_statements_comparisons.py
@@ -0,0 +1,27 @@
+# max
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(300,40,500))
+
+# min
+
+def min_num(num1, num2, num3):
+    if num1 <= num2 and num1 <= num3:
+        return num1
+    elif num2 <= num1 and num2 <= num3:
+        return num2
+    else:
+        return num3
+
+
+print(min_num(300,40,500))
+
+# comparisons operators are : <= >= !=
