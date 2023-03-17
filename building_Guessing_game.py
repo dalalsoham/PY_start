452eef9466df02eebfdfefac2567fa954d3f7823
mr_sd_jack_003 <barson0habra@gmail.com>
2023-03-18 00:00:57 +0530

    update the code with new things add in this code

--- building_Guessing_game.py
+++ building_Guessing_game.py
@@ -1,7 +1,18 @@
 secret_word = "giraffe"
 guess =""
+guess_count = 0
+guess_limit = 3
+out_off_guesses = False
 
-while guess != secret_word:
-    guess = input("Enter guess: ")
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-print("You win!!!")
+
+if out_of_guesses:
+    print("Out of guesses, You lose!!!")
+else:
+    print("You win!!!")
