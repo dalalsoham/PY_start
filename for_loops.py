7ea6d0ace7a395678650d8d0eea718d394a717cb
mr_sd_jack_003 <barson0habra@gmail.com>
2023-03-18 20:21:17 +0530

    upload done ✅

---
+++ for_loops.py
@@ -0,0 +1,14 @@
+
+friends = ["jim", "james", "Kevin"]
+# len(friends)
+# for letter in friends:
+    # print(friends)
+    # print(letter)
+
+# for index in range(len(friends)):
+#     print(friends[index])
+for index in range(5):
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("not first")
